acca6688976ad4928bfbe93bd40b3523a363ed36
bitsy <bitsydoge@proton.me>
2026-01-02 02:31:08 +0100

    misc

--- Test/main.c
+++ Test/main.c
@@ -257,7 +257,6 @@ int BF_Main(void) {
 	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_RESIZABLE | FLAG_MSAA_4X_HINT);
 	InitWindow(1024, 768, "Clay - Raylib Renderer Example");
 
-
 	// ==================================
 	// Load resources
 	gProfilePicture = LoadTexture("resources/profile-picture.png");
